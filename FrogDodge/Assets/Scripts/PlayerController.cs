19c34527313ce90385fbf10aba8c9f8e2bf3d65e
OrphenD <AQuach89@gmail.com>
2015-12-21 16:10:03 -0800

    Added colors to some items and hitbox indicators -added movement to the 'frog'

---
+++ FrogDodge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody rg;
+	public float speed;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rg = GetComponent<Rigidbody>();
+		rg.velocity = movement * speed;
+	}
+}
