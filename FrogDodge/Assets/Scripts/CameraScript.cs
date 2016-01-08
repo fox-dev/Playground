ba18ebd6a78a338ad4515387b3c915c842f9bea5
Andrew Abriam <a_abriam@outlook.com>
2016-01-07 17:33:17 -0800

    Added player rotation when moving
    Just playing around with player movement

---
+++ FrogDodge/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(player.transform.position.x + 10f, player.transform.position.y + 15f, player.transform.position.z - 10f);
+	
+	}
+}
--- FrogDodge/Assets/Scripts/PlayerController.cs
+++ FrogDodge/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,61 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	Rigidbody rg;
-    public float speed = 0.1f;
-    Vector3 dest;
+    Vector3 startPos, endPos;
+    Quaternion startRot, endRot;
+
+    private GameObject endRotation;
+
+    private float lerp = 0, duration = 5f;
+    public float speed, rotSpeed, moveDistance;
+    public int rotationInDegrees;
+
+    //Animator anim;
+
+    //int moveHash = Animator.StringToHash("Move");
 
     void Start()
     {
-        dest = new Vector3(transform.position.x, 0.525f, transform.position.z);
+        startPos = transform.position;
+        endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+       
+        startRot = transform.rotation;
+
+        endRotation = new GameObject();
+        endRotation.transform.rotation = transform.rotation;
+
+       // anim = GetComponent<Animator>();
+      
+
+
+
     }
 
     void Update()
 	{
-        
+        print(endRotation.transform.rotation.x);
 
         //float moveHorizontal = Input.GetAxis("Horizontal");   
-        float moveVertical = Input.GetAxis("Vertical");
-        float step = speed * Time.deltaTime;
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        lerp += Time.deltaTime / duration;
+     
 
         if (Input.GetKeyDown("space"))
         {
-            dest = new Vector3(transform.position.x, 0.525f, transform.position.z + 3);
+            endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveDistance);
+            endRotation.transform.rotation *= Quaternion.Euler(rotationInDegrees, 0, 0);
+
+            //anim.SetTrigger(moveHash);
+           
+          
         }
 
-        transform.position = Vector3.Lerp(transform.position, dest, step);
+       // float move = Input.GetAxis("Vertical");
+       // anim.SetFloat("Speed", move);
+
+        transform.position = Vector3.MoveTowards(transform.position, endPos, speed*Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, endRotation.transform.rotation, rotSpeed * Time.deltaTime);
 
         //Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);
         //Vector3 movement = new Vector3(0.0f, 0.0f, 1f);
