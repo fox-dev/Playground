6e08e5ae97b07e56510dbf1efc3952f65c79dbb8
OrphenD <AQuach89@gmail.com>
2016-01-12 14:02:25 -0800

    Fixed Lanes

--- FrogDodge/Assets/Scripts/DestroyByBoundary.cs
+++ FrogDodge/Assets/Scripts/DestroyByBoundary.cs
@@ -6,11 +6,7 @@ public class DestroyByBoundary : MonoBehaviour {
 	Rigidbody rg;
     public GameObject player;
     public GameObject frontRoad;
-	public GameObject frontRoadR;
-	public GameObject frontRoadL;
     public GameObject[] roads;
-	public GameObject[] roadsR;
-	public GameObject[] roadsL;
 
     public GameObject obstacleCube, obstacleSphere;
 
@@ -34,18 +30,5 @@ public class DestroyByBoundary : MonoBehaviour {
 
         }
 
-		if (other.tag == "RoadR")
-		{
-			frontRoadR = other.gameObject;
-			other.transform.position = new Vector3(0, 0, other.transform.position.z + 40);
-		
-		}
-
-		if (other.tag == "RoadL")
-		{
-			frontRoadL = other.gameObject;
-			other.transform.position = new Vector3(0, 0, other.transform.position.z + 40);
-
-		}
 	}
 }
