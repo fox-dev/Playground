ccb7f3babb00905e1caa35a270322bab49b3733e
Andrew Abriam <a_abriam@outlook.com>
2016-02-08 01:05:59 -0800

    Changed player anim and camera changes
    Player and Camera follow changes

--- FrogDodge/Assets/Scripts/CameraScript.cs
+++ FrogDodge/Assets/Scripts/CameraScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 
     public GameObject player;
+    private Vector3 followPos;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +15,10 @@ public class CameraScript : MonoBehaviour {
 	void Update () {
 
         //transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z - 36.9528f);
-       transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z - 34f);
+        //transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z - 81);
+        Vector3 follow = new Vector3(transform.position.x, transform.position.y, player.transform.position.z - 76f); 
+        followPos = Vector3.Lerp(transform.position, follow, Time.deltaTime);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, followPos.z);
 
     }
 }
